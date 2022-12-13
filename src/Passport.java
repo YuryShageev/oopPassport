c99212765b6d2d6f9668801b9ee4e63851c75d45
YuryShageev <schageev.yur@yandex.ru>
2022-12-13 16:04:19 +0300

    Initial commit

---
+++ src/Passport.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class Passport {
+    private String number;
+    private String firstName;
+    private String secondName;
+    private String middleName;
+    private LocalDate birthday;
+}
